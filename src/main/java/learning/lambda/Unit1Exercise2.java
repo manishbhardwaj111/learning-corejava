ad1bb29835d35464d85efaaff36fc69fbacc1494
manishbhardwaj111 <manishbhardwaj111@gmail.com>
2017-11-26 22:06:50 +0530

    add new method for sorted order

--- src/main/java/learning/lambda/Unit1Exercise2.java
+++ src/main/java/learning/lambda/Unit1Exercise2.java
@@ -1,6 +1,7 @@
 package learning.lambda;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -8,7 +9,7 @@ public class Unit1Exercise2 {
 
 	public static void main(String[] args) {
 
-		List<Integer> numbers=Arrays.asList(2,3,4,7,8);
+		List<Integer> numbers=Arrays.asList(7,8,2,3,4,5);
 		// Create a method that prints all elements
 		System.out.println("Prints all elements");
 		printAll(numbers, n -> true);
@@ -18,6 +19,10 @@ public class Unit1Exercise2 {
 		//Create a method that prints all even element
 		System.out.println("Prints all even elements");
 		printAll(numbers, n -> n%2 == 0);
+		// Create a method that prints all elements in sorted order
+		System.out.println("Prints all elements in sorted order");
+		Collections.sort(numbers, (n1,n2) -> n1.compareTo(n2));
+		printAll(numbers, n -> true);		
 	}
 
 	private static void printAll(List<Integer> number,Predicate<Integer> condition) {
